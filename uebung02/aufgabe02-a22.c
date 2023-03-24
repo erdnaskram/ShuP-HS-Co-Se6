3eb08f0597a24e6e39b5101f72497bf7e0f7bafe
Jonas Heß <mail@jonashess.net>
2023-03-24 23:23:57 +0100

    .gitignore hinzugefügt (diesmal wirklich...), Aufgaben der Prozesse weitergeführt (wait()-Aufrufe fehlen noch)

--- uebung02/aufgabe02-a22.c
+++ uebung02/aufgabe02-a22.c
@@ -2,11 +2,20 @@
 // Created by joni134 on 24.03.23.
 //
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 int main (int argc, char *argv[]) {
+	if(argc<5) {
+		printf("Geben Sie mindestens 4 Aufrufparameter an!\n");
+		return 1;
+	}
+
 	int mainpid = getpid();
-	printf("Main-Process-ID: %i\n", mainpid);
+	printf("Parent-Process-ID: %i\n", mainpid);
+	printf("Aufrufparameter 1: %s\n",argv[1]);
+
 	int pc1,pc2,pc3,processFunction;
 	pc1 = fork();
 	if(getpid() == mainpid) {
@@ -35,14 +44,24 @@ int main (int argc, char *argv[]) {
 		case 1:
 		case 2:
 			// C1+C2
-			printf("Do C1/C2\n");
-			break;
+			printf("Aufrufparameter %i: %s\n",processFunction+1,argv[processFunction+1]);
+			printf("Child%i-Process-ID: %i\n",processFunction, getpid());
+			while(1) { sleep(1); }
+			break; // ToDo: Rest implementieren!
 		case 3:
 			// C3
-			printf("Do C3\n");
-			break;
+			printf("Aufrufparameter %i: %s\n",processFunction+1,argv[processFunction+1]);
+			printf("Child%i-Process-ID: %i\n",processFunction, getpid());
+			sleep(1);
+			exit(2);
+			break; // ToDo: Rest implementieren!
 		default:
 			// Parent
-			printf("Do Parent\n");
+			sleep(2);
+			kill(pc1,15); // 15 = SIGTERM!
+			kill(pc2,9); // 9 = SIGTERM!
+			kill(pc3,9); // 9 = SIGTERM!
+			// ToDo: Endestatus für C1, C2, C3 holen und ausgeben
+			exit(0);
 	}
 }
