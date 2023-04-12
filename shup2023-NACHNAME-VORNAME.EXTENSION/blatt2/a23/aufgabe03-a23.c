22a4ea40c1ff1fdea617061eec13302d63886512
Zoe Schulz <zoe.schulz@mail.de>
2023-04-12 11:12:20 +0200

    A23 implementiert

---
+++ shup2023-NACHNAME-VORNAME.EXTENSION/blatt2/a23/aufgabe03-a23.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sigtermhandler(int);
+
+int counter = 0;
+
+int main (int argc, char *argv[]){
+	printf("Programm gestartet\n");
+	printf("Bei 3-maligem SIGTERM oder SIGINT beendet sich das Programm\n");
+
+	signal(SIGTERM, sigtermhandler);
+	signal(SIGINT, sigtermhandler);
+
+	while(1);
+
+
+}
+
+void sigtermhandler(int signb){
+	counter++;
+	printf("Signal: %i eingetroffen\n", signb);
+	if(counter == 3){
+		printf("Programm beendet sich");
+		exit(5);
+	}
+}
