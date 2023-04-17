f5a011cf7a4e9ae1b7a6542d54e44e86018fbd05
erdnaskram <erdnaskram@gmx.de>
2023-04-17 19:24:12 +0200

    Treffen 17.04. übergabe an studis :)

--- shup2023-NACHNAME-VORNAME.EXTENSION/blatt2/a25/aufgabe02-a25.c
+++ shup2023-NACHNAME-VORNAME.EXTENSION/blatt2/a25/aufgabe02-a25.c
@@ -4,6 +4,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <wait.h>
 
 
 void setColor(int color) {
@@ -22,8 +25,66 @@ void printPrompt() {
 }
 
 int main(int argc, char *argv[]) {
-    printPrompt();
+    char input[1000];
 
+
+    while (1) {
+        printPrompt();
+        fgets(input, 1000, stdin);
+        input[strlen(input) - 1] = '\0';
+
+        if (strcmp(input, "schluss") == 0) {
+            printf("ich zerstöre mich jetzt selbst!\n");
+            break;
+        }
+
+
+        int spaces = 0;
+        for (int i = 0; i < strlen(input); i++) {
+            if (input[i] == ' ') {
+                spaces++;
+            }
+        }
+
+        char **params = (char **) malloc((spaces + 2) * sizeof(char *));
+
+        params[0] = strtok(input, " ");
+
+        for (int i = 1; i < spaces + 1; ++i) {
+            params[i] = strtok(NULL, " ");
+            printf("%s\n", params[i]);
+        }
+        params[spaces + 1] = NULL;
+        char *command = params[0];
+
+        printf("Command %s\n", command);
+
+        if (!(command[0] == '/'
+            || (command[0] == '.' && command[1] == '/')
+            || (command[0] == '.' && command[1] == '.' && command[2] == '/'))) {
+
+              // /bin:/usr/bin:/usr/local/bin
+
+        }
+
+        int child = fork();
+
+        if (child != -1) {
+            if (child == 0) {
+
+
+                execv(command, params);
+                printf("Error File not found: %s\n", command);
+                exit(1);
+            } else {
+                int status;
+                pid_t pid = wait(&status);
+                printf("Kindprozess mit pid %i beendet mit Status %i\n", (int) pid, (int) status);
+            }
+        }
+        free(params);
+
+    }
 
 
 }
