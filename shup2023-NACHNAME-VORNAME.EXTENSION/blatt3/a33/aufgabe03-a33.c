263cdf007754f63bd90eeb37a868aa35160087e4
Zoe Schulz <zoe.schulz@mail.de>
2023-04-28 18:41:44 +0200

    A33 Children + Druckerspool + deren Synchronisation implementiert

--- shup2023-NACHNAME-VORNAME.EXTENSION/blatt3/a33/aufgabe03-a33.c
+++ shup2023-NACHNAME-VORNAME.EXTENSION/blatt3/a33/aufgabe03-a33.c
@@ -41,12 +41,13 @@ void signal_sem(int semid, int semnum) {
 
 
 int main() {
+	printf("Parent-Prozess-ID: %d\n", getpid());
 
     signal(SIGTERM, sigtermhandler);
     signal(SIGINT, sigtermhandler);
 
     // 	Shared Memory-Bereich erzeugen
-    // 	5 int-Werte (jewiels SharedMemoryID für 1 Child) + 2 int-Werte (next to read + next to write) = 7 int-Werte
+    // 	5 int-Werte (jeweils SharedMemoryID für 1 Child) + 2 int-Werte (next to read + next to write) = 7 int-Werte
     int shm_id = shmget(IPC_PRIVATE, 7 * sizeof(int), 0777 | IPC_CREAT);
     if (shm_id < 0) {
         printf("Fehler beim Erzeugen des Shared Memory-Bereichs\n");
@@ -66,8 +67,12 @@ int main() {
         exit(1);
     }
 
-    semctl(semid, 0, SETALL, 0); //TODO: Hier haben wir aufgehört!
-
+	//0. Sem = mutex
+    semctl(semid, 0, SETVAL, 1);
+	//1. Sem = erzeuger
+	semctl(semid, 1, SETVAL, 5);
+	//2. Sem = verbraucher
+	semctl(semid, 2, SETVAL, 0);
 
     int spooler = fork();
 
@@ -75,25 +80,28 @@ int main() {
         printf("Fehler beim Erzeugen des Spooler-Prozesses");
         return 1;
     } else if (spooler == 0) {
-        printf("Spoooooooler");
+        printf("Spoooooooler\n");
 
         int *shared_mem = (int *) shmat(shm_id, NULL, 0);
 
-
-        while (run) { // Laut Wißmann kein "sauberes" beenden notwendig, 
+        while (run) { // Laut Wißmann kein "sauberes" beenden notwendig,
 		      // Hinweis, dass noch Geschriebenes nicht mehr gedruckt wird, reicht.
             int *nextToRead = &shared_mem[6];
+
+			wait_sem(semid, 2); //Warten bis etwas in Druckwarteschlange steht
             int sharedMemChildID = shared_mem[*nextToRead];
-            //TODO: SYNCHRONISIEREN + an Drucker senden
+			signal_sem(semid, 1); //einen Platz in Druckerwarteschlange frei geben
 
 
+			printf("Spooler: Shared Memory of child ID: %d\n", sharedMemChildID);
+
+            //TODO: an Drucker senden
 
             //im Ringspeicher eins weiter zählen
             (*nextToRead)++;
             if (*nextToRead == 5) {
                 *nextToRead = 0;
             }
-
         }
 
 	//TODO: Hinweis, dass Geschriebenes nicht mehr gedruckt wird!
@@ -111,7 +119,7 @@ int main() {
         printf("Fehler beim Erzeugen des Drucker1-Prozesses");
         return 1;
     } else if (drucker1 == 0) {
-        printf("Drucker1");
+        printf("Drucker1\n");
         return 0;
     }
 
@@ -122,7 +130,7 @@ int main() {
         printf("Fehler beim Erzeugen des Drucker2-Prozesses");
         return 1;
     } else if (drucker2 == 0) {
-        printf("Drucker2");
+        printf("Drucker2\n");
         return 0;
     }
 
@@ -138,7 +146,6 @@ int main() {
             printf("Fehler beim Erzeugen des Kindprozesses\n");
             return 1;
         } else if (pid == 0) { // wennn pid = 0 ist, ist es ein Kindprozess
-            //sychronisieren
             int *shared_mem = (int *) shmat(shm_id, NULL, 0);
 
             int pages = rand() % 5 + 2;
@@ -151,7 +158,22 @@ int main() {
                 shared_child_mem[i] = rand();// Schreiben des Druckinhalts in den Speicherplatz
             }
 
-            *shared_mem = shm_child_id;//TODO: SYNCHRONISIEREN + next to write bedenken!
+			wait_sem(semid, 1); //einen Platz in Druckerwarteschlange belegen
+			wait_sem(semid, 0); //auf Schreibzugriff warten
+
+            int *nextToWrite = &shared_mem[5];
+            shared_mem[*nextToWrite] = shm_child_id;
+
+            //im Ringspeicher eins weiter zählen
+            (*nextToWrite)++;
+            if (*nextToWrite == 5) {
+                *nextToWrite = 0;
+            }
+
+			printf("Child: Shared Memory of Child ID: %d\n", shm_child_id);
+
+			signal_sem(semid, 0); //Schreibzugriff abgeben
+			signal_sem(semid, 2); //Spooler über Druckauftrag informieren
 
             //detach shared memory
             shmdt(shared_mem);
