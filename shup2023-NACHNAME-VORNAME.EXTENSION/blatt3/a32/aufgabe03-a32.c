4703b5f1a3eb904e8a0cc01220552363428680f8
Zoe Schulz <zoe.schulz@mail.de>
2023-05-07 14:19:13 +0200

    A32 bearbeitet: angepasst und readme hinzugefügt

--- shup2023-NACHNAME-VORNAME.EXTENSION/blatt3/a32/aufgabe03-a32.c
+++ shup2023-NACHNAME-VORNAME.EXTENSION/blatt3/a32/aufgabe03-a32.c
@@ -12,50 +12,64 @@ int main(int argc, char *argv[]) {
 
 	//Prüfung auf zwei übergebene Argumente
     if (argc != 3) {
-        printf("Usage: %s <Anzahl Schleifendurchläufe> <Seed>\n", argv[0]);
+        perror("korrekter Aufruf: %s <Anzahl Schleifendurchläufe> <Seed>\n", argv[0]);
         return 1;
     }
 
     //Anzahl Schleifendurchläufe und Seed aus den Argumenten auslesen
-    int n = atoi(argv[1]);
+    int anzahlDurchlaeufe = atoi(argv[1]);
     int seed = atoi(argv[2]);
 
     //Shared Memory-Bereich erzeugen
     int shm_id = shmget(IPC_PRIVATE, sizeof(int), 0777 | IPC_CREAT);
     if (shm_id < 0) {
-        printf("Fehler beim Erzeugen des Shared Memory-Bereichs\n"); //Oder perror?
+        perror("Fehler beim Erzeugen des Shared Memory-Bereichs\n");
         return 1;
     }
     printf("Shared Memory ID: %d\n", shm_id);
+
     //Zufallsgenerator mit Seed initialisieren
     srand(seed);
 
     //Kindprozess erzeugen
     int pid = fork();
     if (pid == -1) {
-        printf("Fehler beim Erzeugen des Kindprozesses\n");
+        perror("Fehler beim Erzeugen des Kindprozesses\n");
         return 1;
     } else if (pid == 0) {
-        //Kindprozess liest Zufallszahlen aus Shared Memory
+        //Kindprozess
+
+		//Shared Memory einblenden
         int *shared_mem = (int*) shmat(shm_id, NULL, 0);
-        for (int i = 0; i < n; i++) {
-            printf("Kindprozess: %d\n", *shared_mem);
+
+		//Zufallszahlen aus Shared Memory lesen
+        for (int i = 0; i < anzahlDurchlaeufe; i++) {
+            printf("Kindprozess liest: %d\n", *shared_mem);
         }
+
+		//Shared Memory ausblenden
         shmdt(shared_mem);
     } else {
-        //Vaterprozess schreibt Zufallszahlen in Shared Memory
+        //Vaterprozess
+
+		//Shared Memory einblenden
         int *shared_mem = shmat(shm_id, NULL, 0);
-        for (int i = 0; i < n; i++) {
+
+		//Zufallszahlen in Shared Memory schreiben
+        for (int i = 0; i < anzahlDurchlaeufe; i++) {
             *shared_mem = rand();
             printf("Vaterprozess: %d\n", *shared_mem);
         }
+
+		//Shared Memory ausblenden
         shmdt(shared_mem);
 
-	// Wait for child-process (so that the shared memory doesn't get removed before it is executed)
-	int status;
-	wait(&status);
+		//auf Kindprozess warten
+		int status;
+		pid_t pid = wait(&status);
+		printf("Kindprozess mit pid %i beendet mit Status %i\n", (int) pid, (int) status;
 
-        // Shared Memory-Bereich freigeben
+        //Shared Memory-Bereich löschen
         shmctl(shm_id, IPC_RMID, NULL);
     }
 
